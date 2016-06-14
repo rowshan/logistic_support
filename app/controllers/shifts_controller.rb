5c9f83c57a290651cb90690f900a0b0fe2cf619f
rowshansa <rowshan_sathi@yahoo.com>
2016-06-14 10:43:08 +0200

    savepoint2

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -16,6 +16,9 @@ class ShiftsController < ApplicationController
   # POST /shifts
   def create
     @shift = Shift.new(shift_params)
+    if shift_params[:time_window_id]
+      @shift.time_window=TimeWindow.find(shift_params[:time_window_id])
+    end
 
     if @shift.save
       render json: @shift, status: :created
@@ -26,6 +29,22 @@ class ShiftsController < ApplicationController
 
   # PATCH/PUT /shifts/1
   def update
+    logger.debug 'ORDER_PARAMS:' + shift_params.inspect
+    params_h = shift_params.to_h
+    if (params_h.include?(:name))
+      @shift=Shift.find(params_h.delete(:name))
+    end
+
+    # if (params_h.include?(:start_time))
+    #   @shift=Shift.find(params_h.delete(:start_time))
+    # end
+    #
+    # if (params_h.include?(:end_time))
+    #   @shift=Shift.find(params_h.delete(:end_time))
+    # end
+
+
+
     if @shift.update(shift_params)
       render json: @shift
     else
@@ -46,6 +65,6 @@ class ShiftsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def shift_params
-      json_api_params(Shift, [:name, :start_time, :end_time, :send_time, :enabled]).fetch(:attributes, {})
+      json_api_params(Shift, [:name,:time_window_id, :start_time, :end_time, :send_time, :enabled]).fetch(:attributes, {})
     end
 end
--- app/serializers/time_window_serializer.rb
+++ app/serializers/time_window_serializer.rb
@@ -3,6 +3,6 @@ class TimeWindowSerializer < ApplicationSerializer
 
   has_many :time_windows
   def url
-    url_helpers.time_window_path( object)
+    url_helpers.time_window_path(object)
   end
 end
