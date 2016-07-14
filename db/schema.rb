aa61efa359af03dbc95feffc87aabc8063ee9673
rowshansa <rowshan_sathi@yahoo.com>
2016-07-14 17:22:15 +0200

    schedule table  added for ticket JONNY2-91

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160622072132) do
+ActiveRecord::Schema.define(version: 20160714121801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +56,14 @@ ActiveRecord::Schema.define(version: 20160622072132) do
     t.text     "phone_no"
     t.text     "address"
     t.uuid     "tenant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "schedules", id: :uuid, default: -> { "uuid_generate_v4()" }, force: :cascade do |t|
+    t.date     "date"
+    t.uuid     "shift_id"
+    t.uuid     "driver_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
