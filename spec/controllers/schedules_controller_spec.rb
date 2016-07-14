e77545fd9743bb7a1c9fb63c54464fd2d30325cf
rowshansa <rowshan_sathi@yahoo.com>
2016-07-14 15:23:35 +0200

    Controller spec for ticket JONNY2-91

---
+++ spec/controllers/schedules_controller_spec.rb
@@ -0,0 +1,108 @@
+require 'rails_helper'
+
+
+RSpec.describe SchedulesController, type: :controller do
+  before {
+    controller_stub_token_authentication
+  }
+
+  let(:valid_session) { {} }
+
+
+  describe "GET index" do
+    let!(:schedules) { create_list :schedule, rand(2..10) }
+    it "assigns all schedules as @schedules" do
+      get :index, params: {}, session: valid_session
+      expect(assigns(:schedules)).to eq(schedules)
+    end
+  end
+
+  describe "GET show" do
+    let!(:schedule) { create :schedule }
+    it "assigns the requested schedule as @schedule" do
+      get :show, params: {:id => schedule.to_param}, session: valid_session
+      expect(assigns(:schedule)).to eq(schedule)
+    end
+  end
+
+  describe "POST create" do
+    let!(:shift) { create :shift }
+    let!(:driver) { create :driver }
+
+    describe "with valid params" do
+      it "creates a new Schedule" do
+        expect {
+          post :create, params:{:shift_id=> shift.to_param,:driver_id=> driver.to_param}.merge(
+              json_api_params(Schedule, attributes_for(:schedule).update(shift_id:plant.to_param,
+                                                                      :driver_id=> driver.to_param))
+          ), session: valid_session
+        }.to change(Schedule, :count).by(1)
+      end
+
+      it "assigns a newly created schedule as @schedule" do
+        post :create, params:{:plant_id=> plant.to_param}.merge(
+            json_api_params(Schedule, attributes_for(:schedule).update(shift_id:shift.to_param,
+                                                                    :driver_id=> driver.to_param))
+        ), session: valid_session
+        expect(assigns(:schedule)).to be_a(Schedule)
+        expect(assigns(:schedule)).to be_persisted
+      end
+    end
+
+    describe "with invalid params" do
+      it "assigns a newly created but unsaved schedule as @schedule" do
+        post :create, params: {:plant_id=> plant.to_param}.merge(
+            json_api_params(Schedule, attributes_for(:schedule)).update(
+                name:nil,start_time:nil, end_time:nil,send_time:nil)), session: valid_session
+        expect(assigns(:schedule)).to be_a_new(Schedule)
+      end
+    end
+  end
+
+
+  describe "PUT update" do
+    let!(:schedule) { create :schedule  }
+    let(:new_attributes) { attributes_for(:schedule)}
+
+    describe "with valid params" do
+      it "updates the requested Schedule" do
+        put :update, params: {:id => schedule.to_param}.update(
+            json_api_params(Schedule, new_attributes)), session: valid_session
+        schedule.reload
+        new_attributes.slice(:id,  :driver_id,:shift_id).each do |attr, val|
+          expect(schedule.send(attr)).to eq(val)
+        end
+        new_attributes.slice(:start_time, :end_time).each do |attr, val|
+          expect(schedule.send(attr).strftime("%H:%M")).to eq(val)
+        end
+      end
+
+      it "assigns the requested schedule as @schedule" do
+        put :update, params: {:id => schedule.to_param}.update(
+            json_api_params(Schedule, new_attributes)), session: valid_session
+        expect(assigns(:schedule)).to eq(schedule)
+      end
+    end
+
+    describe "with invalid params" do
+      it "assigns the schedule as @schedule" do
+        put :update, params: {:id => schedule.to_param}.update(
+            json_api_params(Schedule, new_attributes.update(
+                name:nil,start_time:nil, end_time:nil))
+        ), session: valid_session
+        expect(assigns(:schedule)).to eq(schedule)
+      end
+
+    end
+  end
+
+  describe "DELETE destroy" do
+    let!(:schedule) { create :schedule }
+    it "destroys the requested schedule" do
+      expect {
+        delete :destroy, params: {:id => schedule.to_param}, session: valid_session
+      }.to change(Schedule, :count).by(-1)
+    end
+  end
+
+end
