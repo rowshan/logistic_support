19d3ac2f5a0eb205306140e435dd565230862671
rowshansa <rowshan_sathi@yahoo.com>
2016-07-14 17:24:04 +0200

    small fixes for model association for ticket JONNY2-91

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,7 +1,7 @@
 class Shift < ApplicationRecord
- has_and_belongs_to_many :time_windows
- belongs_to :plant
- has_many :schedules
+  has_and_belongs_to_many :time_windows
+  belongs_to :plant
+  has_many :schedules
 
- validates_presence_of :name, :start_time, :end_time, :plant_id
+  validates_presence_of :name, :start_time, :end_time, :plant_id
 end
