df068ba8bb6a67707b57e40c3ae2530679d612e9
rowshansa <rowshan_sathi@yahoo.com>
2016-07-28 09:26:51 +0200

    tenant enforcement for  ticket JONNY2-59

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,9 +1,10 @@
 class PlantsController < ApplicationController
   before_action :set_plant, only: [:show, :update, :destroy]
+  before_action :validate_tenant, only: [:show, :update, :destroy]
 
   # GET /plants
   def index
-    @plants = Plant.all
+    @plants = Plant.where(tenant: current_context.tenant_id).all
 
     render json: @plants
   end
@@ -39,13 +40,23 @@ class PlantsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_plant
-      @plant = Plant.find(params[:id])
+  # Use callbacks to share common setup or constraints between actions.
+  def set_plant
+    @plant = Plant.find(params[:id])
+  end
+
+  def validate_tenant
+    if @plant and !@plant.tenant_id.eql?(current_context.tenant_id)
+      raise ApiM8::Exceptions::TenantMismatch.new
     end
 
-    # Only allow a trusted parameter "white list" through.
-    def plant_params
-      json_api_params(Plant,[:company,:email,:phone_no,:address,:tenant_id]).fetch(:attributes, {})
+    if @plant and plant_params.include?(:tenant_id) and !@plant.tenant_id.eql?(plant_params[:tenant_id])
+      raise ApiM8::Exceptions::TenantMismatch.new
     end
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def plant_params
+    json_api_params(Plant, [:company, :email, :phone_no, :address, :tenant_id]).fetch(:attributes, {})
+  end
 end
