f59f861e32fa1ca19222d79c88fbd7682aebfffb
mihaimuntenas <mihai.muntenas@gmail.com>
2016-09-12 16:23:42 +0200

    fixed seeds for Driver

--- db/seeds/4_drivers.rb
+++ db/seeds/4_drivers.rb
@@ -1,7 +1,7 @@
 ps = Plant.all.map(&:id)
 drivers = (0..20).to_a.collect do |i|
   {
-      plant_id: ps.sample,
+      plant_id: ps.sample, user_id: SecureRandom.uuid,
       first_name: SecureRandom.hex, last_name: SecureRandom.hex,
       phone_no: (1..13).to_a.map{ rand(0..9) }.join,
   }
